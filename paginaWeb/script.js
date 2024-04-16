377454ca7a7dcaa0d0fb63d739c90dc559702687
patrick03122024 <contato.claircommerce@gmail.com>
2024-04-16 19:44:41 -0300

    commit1

--- paginaWeb/script.js
+++ paginaWeb/script.js
@@ -3,8 +3,8 @@ const desligarButton = document.getElementById('desligarButton');
 const restartButton = document.getElementById('restartButton');
 const producaoDisplay = document.getElementById('producaoDisplay');
 
-var urlPost = 'https://leanwebsensorserver.onrender.com/chaves'
-var urlGet = 'https://leanwebsensorserver.onrender.com/producao'
+var urlPost = 'https://leanwebsensor-ei8h.onrender.com/chaves'
+var urlGet = 'https://leanwebsensor-ei8h.onrender.com/producao'
 
 function receiverRequest(){
     fetch(urlGet, {
